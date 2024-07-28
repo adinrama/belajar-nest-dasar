e8bc2d7b938354e13a97f22bd5e3cce54c69109d
adinrama <adinrama@student.ub.ac.id>
2024-07-28 03:58:46 +0000

    need further handling

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,16 @@ import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import * as mustache from 'mustache-express'
 import * as cookieParser from 'cookie-parser'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.use(cookieParser('CONFIDENTIAL'))
-
   app.set('views', __dirname, '/../views')
   app.set('view engine', 'html')
   app.engine('html', mustache())
 
-  await app.listen(3000)
+  const configService = app.get(ConfigService)
+  await app.listen(configService.get('PORT'))
 }
-
 bootstrap()
